cb01aec04fa8fc2b9734de3c7d30344dfd768c12
dimassabimanyy <dmsabmny@gmail.com>
2020-09-27 07:22:36 +0700

    get and show jobs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import useFetchJobs from "./useFetchJobs";
+import Job from "./Job";
 
 export default function App() {
-  const { jobs, loading, error } = useFetchJobs();
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
   return (
-    <Container>
+    <Container className="my-4">
+      <h1 className="mb-4">Github Jobs</h1>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error, Try refreshing...</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
     </Container>
   );
 }
